c245e80b1f497abcb7a6a8cca95b2f0d1d4a2bea
Ayelen Lopez Bizon <alopezbizon@fi.uba.ar>
2020-12-17 14:29:44 -0300

    Agrego pruebas de Personaje y corrijo el nombre de otras.

--- src/main/test/edu/fiuba/algo3/modelo/personaje/PersonajeTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/personaje/PersonajeTest.java
@@ -1,37 +1,146 @@
 package edu.fiuba.algo3.modelo.personaje;
 
-import org.junit.jupiter.api.Test;
+import edu.fiuba.algo3.modelo.tablero.Posicion;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class PersonajeTest {
 
     @Test
-    public void testCrearPersonaje_deberiaTenerElLapizArriba() {
+    public void testPersonajeCreacion_deberiaTenerElLapizArriba() {
         Personaje personaje = Personaje.darPersonaje();
+
         assertFalse(personaje.dibujar());
     }
 
     @Test
-    public void testCrearPersonaje_cuandoSubeElLapiz_deberiaMantenerElLapizArriba() {
+    public void testPersonajeSubirElLapiz_deberiaMantenerElLapizArriba() {
         Personaje personaje = Personaje.darPersonaje();
+
         personaje.subirLapiz();
+
         assertFalse(personaje.dibujar());
     }
 
     @Test
-    public void testPersonaje_cuandoBajaElLapiz_deberiaTenerElLapizAbajo() {
+    public void testPersonajeBajarLapiz_deberiaTenerElLapizAbajo() {
         Personaje personaje = Personaje.darPersonaje();
+
         personaje.bajarLapiz();
+
         assertTrue(personaje.dibujar());
     }
 
     @Test
-    public void testPersonaje_cuandoBajaElLapizYLoVuelveASubir_deberiaTenerElLapizArriba() {
+    public void testPersonajeBajarYSubirLapiz_deberiaTenerElLapizArriba() {
         Personaje personaje = Personaje.darPersonaje();
+
         personaje.bajarLapiz();
+        assertTrue(personaje.dibujar());
         personaje.subirLapiz();
+
         assertFalse(personaje.dibujar());
+    }
+
+    @Test
+    public void testPersonajeDibujar_deberiaDevolverTrueCuandoElLapizEstaBajo() {
+        Personaje personaje = Personaje.darPersonaje();
+
+        personaje.bajarLapiz();
+
+        assertTrue(personaje.dibujar());
+    }
+
+    @Test
+    public void testPersonajeDibujar_deberiaDevolverFalseCuandoElLapizEstaArriba() {
+        Personaje personaje = Personaje.darPersonaje();
+
+        personaje.subirLapiz();
+
+        assertFalse(personaje.dibujar());
+    }
+
+    @Test
+    public void testPersonajeMover_cuandoSeLoMueveALaDerecha_deberiaActualizarSuPosicionUnaPosicionHaciaLaDerecha() {
+        Personaje personaje = Personaje.darPersonaje();
+        Posicion posicionInicial = new Posicion(5,5);
+        Posicion posicionFinal = new Posicion(6,5);
+        Posicion posicionParaMovimiento = new Posicion(1,0);
+
+        personaje.inicializarPosicion(posicionInicial);
+        assertTrue(personaje.obtenerPosicion().esIgual(posicionInicial));
+        personaje.mover(posicionParaMovimiento);
+
+        assertTrue(personaje.obtenerPosicion().esIgual(posicionFinal));
+    }
+
+    @Test
+    public void testPersonajeMover_cuandoSeLoMueveALaIzquierda_deberiaActualizarSuPosicionUnaPosicionHaciaLaIzquierda() {
+        Personaje personaje = Personaje.darPersonaje();
+        Posicion posicionInicial = new Posicion(5,5);
+        Posicion posicionFinal = new Posicion(4,5);
+        Posicion posicionParaMovimiento = new Posicion(-1,0);
+
+        personaje.inicializarPosicion(posicionInicial);
+        assertTrue(personaje.obtenerPosicion().esIgual(posicionInicial));
+        personaje.mover(posicionParaMovimiento);
+
+        assertTrue(personaje.obtenerPosicion().esIgual(posicionFinal));
+    }
+
+    @Test
+    public void testPersonajeMover_cuandoSeLoMueveArriba_deberiaActualizarSuPosicionUnaPosicionHaciaArriba() {
+        Personaje personaje = Personaje.darPersonaje();
+        Posicion posicionInicial = new Posicion(5,5);
+        Posicion posicionFinal = new Posicion(5,6);
+        Posicion posicionParaMovimiento = new Posicion(0,1);
+
+        personaje.inicializarPosicion(posicionInicial);
+        assertTrue(personaje.obtenerPosicion().esIgual(posicionInicial));
+        personaje.mover(posicionParaMovimiento);
+
+        assertTrue(personaje.obtenerPosicion().esIgual(posicionFinal));
+    }
+
+    @Test
+    public void testPersonajeMover_cuandoSeLoMueveAbajo_deberiaActualizarSuPosicionUnaPosicionHaciaAbajo() {
+        Personaje personaje = Personaje.darPersonaje();
+        Posicion posicionInicial = new Posicion(5,5);
+        Posicion posicionFinal = new Posicion(5,4);
+        Posicion posicionParaMovimiento = new Posicion(0,-1);
+
+        personaje.inicializarPosicion(posicionInicial);
+        assertTrue(personaje.obtenerPosicion().esIgual(posicionInicial));
+        personaje.mover(posicionParaMovimiento);
+
+        assertTrue(personaje.obtenerPosicion().esIgual(posicionFinal));
+    }
+
+    @Test
+    public void testPersonajeInicializarPosicion_deberiaTenerLaPosicionEnviada() {
+        Personaje personaje = Personaje.darPersonaje();
+        Posicion posicionInicial = new Posicion(5,5);
+
+        personaje.inicializarPosicion(posicionInicial);
+
+        assertTrue(personaje.obtenerPosicion().esIgual(posicionInicial));
+    }
+
+    @Test
+    public void testPersonajeObtenerPosicion_deberiaDevolverLaPosicionActualDelPersonaje() {
+        Personaje personaje = Personaje.darPersonaje();
+        Posicion posicionInicial = new Posicion(5,5);
+        Posicion posicionFinal = new Posicion(7,7);
+
+        personaje.inicializarPosicion(posicionInicial);
+        assertTrue(personaje.obtenerPosicion().esIgual(posicionInicial));
+        personaje.mover(new Posicion(1,0));
+        personaje.mover(new Posicion(1,0));
+        personaje.mover(new Posicion(0,1));
+        personaje.mover(new Posicion(0,1));
+
+        assertTrue(personaje.obtenerPosicion().esIgual(posicionFinal));
     }
 }
